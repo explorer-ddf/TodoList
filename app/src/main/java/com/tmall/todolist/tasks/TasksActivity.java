80f40b123854e2833f3af6ea4cbd3d53574541aa
dongfeng.ddf <dongfeng.ddf@alibaba-inc.com>
2016-07-12 14:48:55 +0800

    init TasksActivity

--- app/src/main/java/com/tmall/todolist/tasks/TasksActivity.java
+++ app/src/main/java/com/tmall/todolist/tasks/TasksActivity.java
@@ -1,13 +1,15 @@
-package com.tmall.todolist;
+package com.tmall.todolist.tasks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tmall.todolist.R;
+
+public class TasksActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tasks);
     }
 }
