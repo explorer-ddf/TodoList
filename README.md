ecf4ac08b32e6103f72585a8580078f85b4f2f38
dongfeng.ddf <dongfeng.ddf@alibaba-inc.com>
2016-07-12 17:34:39 +0800

    实现抽屉效果

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TodoList
+
+#Theme
+http://www.jcodecraeer.com/a/anzhuokaifa/androidkaifa/2015/1122/3712.html
--- app/src/main/java/com/tmall/todolist/tasks/TasksActivity.java
+++ app/src/main/java/com/tmall/todolist/tasks/TasksActivity.java
@@ -1,15 +1,66 @@
 package com.tmall.todolist.tasks;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 import com.tmall.todolist.R;
 
 public class TasksActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+
+        initView();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);// FIXME: 需要与xml中的android:layout_gravity值保持一致
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initView() {
+        // Set up the toolbar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Set up the navigation drawer.
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.tasks_nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.menu_item_todolist:
+                            break;
+                        case R.id.menu_item_statistics:
+                            break;
+                        default:
+                            break;
+                    }
+                    item.setChecked(true);
+                    mDrawerLayout.closeDrawers();
+                    return true;
+                }
+            });
+        }
     }
 }
